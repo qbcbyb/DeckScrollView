5bbb3b57de3a7f0dea09a0de39c288ac118976c2
Hydrant <zhangqiuyun@infohold.com.cn>
2019-10-16 17:33:47 +0800

    flutter pub get

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=D:\Env\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Study\flutter\DeckScrollView\example"
+export "FLUTTER_APPLICATION_PATH=D:\Projects\DriverWallet\Sources\DriverWalletApp\dependencies\deck_scroll_view\example"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build\ios"
