5f2a8b6ed6fd4d63391de771309b2dce13c156cd
Hydrant <zhangqiuyun@infohold.com.cn>
2020-01-19 11:10:32 +0800

    sdk 升级

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\Env\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Projects\DriverWallet\Sources\DriverWalletApp\dependencies\deck_scroll_view\example"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/usr/local/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/projects/driverwalletapp/dependencies/deck_scroll_view/example"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "FLUTTER_FRAMEWORK_DIR=D:\Env\flutter\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/usr/local/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
